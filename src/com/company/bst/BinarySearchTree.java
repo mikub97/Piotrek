2c5dfb626f03dc189b8347e5cc9c4e1cfb55d191
mikub97 <mikub97@gmail.com>
2022-10-07 13:15:08 +0200

    BST

---
+++ src/com/company/bst/BinarySearchTree.java
@@ -0,0 +1,52 @@
+package com.company.bst;
+
+public class BinarySearchTree <T extends Comparable<T>> {
+    class Node<T> {
+        //instance variable of Node class
+        public T data;
+        public Node left;
+        public Node right;
+
+        //constructor
+        public Node(T data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // instance variable of binary tree class
+    public Node root;
+
+    // constructor for initialise the root to null BY DEFAULT
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+    public void insert(T data){
+        Node n = new Node(data);
+        if (root==null) root = n;
+        else{
+            Node tmp = root;
+            Node parent;
+            while (true) { // will break after insertion
+                parent = tmp;
+                if (data.compareTo((T) tmp.data)<0) { // left
+                    tmp = tmp.left;
+                    if (tmp == null) { // if its the leaf
+                        parent.left = n;
+                        break;
+                    }
+                } else {    //right
+                    tmp = tmp.right;
+                    if (tmp==null){
+                        parent.right= n;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
+}
---
+++ test/com/company/bst/BST_test.java
@@ -0,0 +1,24 @@
+package com.company.bst;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class BST_test {
+
+    @Test
+    public void test(){
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        Random r = new Random();
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int n = r.nextInt();
+            nums.add(n);
+            bst.insert(n);
+        }
+        System.out.println("end");
+
+    }
+}
